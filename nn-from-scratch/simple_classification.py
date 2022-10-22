e67b8cb3caf2b2ad49b26235aee3a084db9c0f84
Viet Quang <93778662+hikarihahaha@users.noreply.github.com>
2022-10-22 18:35:56 +0700

    Update simple_classification.py

--- nn-from-scratch/simple_classification.py
+++ nn-from-scratch/simple_classification.py
@@ -1,21 +1,16 @@
-__author__ = 'm.bashari'
 import numpy as np
 from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
-
 
 def generate_data():
     np.random.seed(0)
     X, y = datasets.make_moons(200, noise=0.20)
     return X, y
 
-
 def visualize(X, y, clf):
     # plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
     # plt.show()
     plot_decision_boundary(lambda x: clf.predict(x), X, y)
-    plt.title("Logistic Regression")
-
 
 def plot_decision_boundary(pred_func, X, y):
     # Set min and max values and give it some padding
@@ -31,20 +26,17 @@ def plot_decision_boundary(pred_func, X, y):
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
     plt.show()
-
-
+    
 def classify(X, y):
     clf = linear_model.LogisticRegressionCV()
     clf.fit(X, y)
     return clf
-
 
 def main():
     X, y = generate_data()
     # visualize(X, y)
     clf = classify(X, y)
     visualize(X, y, clf)
-
 
 if __name__ == "__main__":
     main()
