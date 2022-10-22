be16d65b6d5f6ebbd1e62c4feb02d853d0fba202
Viet Quang <93778662+hikarihahaha@users.noreply.github.com>
2022-10-22 18:34:57 +0700

    Update nn_from_scratch.py

--- nn-from-scratch/nn_from_scratch.py
+++ nn-from-scratch/nn_from_scratch.py
@@ -1,5 +1,3 @@
-# %% 1 
-# Package imports 
 import matplotlib.pyplot as plt 
 import numpy as np 
 import sklearn 
@@ -8,22 +6,17 @@ import sklearn.linear_model
 import matplotlib 
  
 # Display plots inline and change default figure size 
-#%matplotlib inline 
 matplotlib.rcParams['figure.figsize'] = (10.0, 8.0) 
  
-# %% 2 
 np.random.seed(3) 
 X, y = sklearn.datasets.make_moons(200, noise=0.20) 
 plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral) 
- 
-# %% 3 
+
 # Train the logistic rgeression classifier 
 clf = sklearn.linear_model.LogisticRegressionCV() 
 clf.fit(X, y) 
- 
-# %% 4 
+
 # Helper function to plot a decision boundary. 
-# If you don't fully understand this function don't worry, it just generates the contour plot below. 
 def plot_decision_boundary(pred_func): 
     # Set min and max values and give it some padding 
     x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5 
@@ -37,22 +30,19 @@ def plot_decision_boundary(pred_func):
     # Plot the contour and training examples 
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral) 
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral) 
- 
-# %% 12 
+
 # Plot the decision boundary 
 plot_decision_boundary(lambda x: clf.predict(x)) 
 plt.title("Logistic Regression") 
  
-# %% 15 
 num_examples = len(X) # training set size 
 nn_input_dim = 2 # input layer dimensionality 
 nn_output_dim = 2 # output layer dimensionality 
  
-# Gradient descent parameters (I picked these by hand) 
+# Gradient descent parameters 
 epsilon = 0.01 # learning rate for gradient descent 
 reg_lambda = 0.01 # regularization strength 
- 
-# %% 7 
+  
 # Helper function to evaluate the total loss on the dataset 
 def calculate_loss(model): 
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2'] 
@@ -68,8 +58,7 @@ def calculate_loss(model):
     # Add regulatization term to loss (optional) 
     data_loss += reg_lambda/2 * (np.sum(np.square(W1)) + np.sum(np.square(W2))) 
     return 1./num_examples * data_loss 
- 
-# %% 8 
+
 # Helper function to predict an output (0 or 1) 
 def predict(model, x): 
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2'] 
@@ -80,14 +69,9 @@ def predict(model, x):
     exp_scores = np.exp(z2) 
     probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) 
     return np.argmax(probs, axis=1) 
- 
-# %% 16 
-# This function learns parameters for the neural network and returns the model. 
-# - nn_hdim: Number of nodes in the hidden layer 
-# - num_passes: Number of passes through the training data for gradient descent 
-# - print_loss: If True, print the loss every 1000 iterations 
+
+# This function learns parameters for the neural network and returns the model.  
 def build_model(nn_hdim, num_passes=20000, print_loss=False): 
- 
     # Initialize the parameters to random values. We need to learn these. 
     np.random.seed(0) 
     W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim) 
@@ -98,7 +82,7 @@ def build_model(nn_hdim, num_passes=20000, print_loss=False):
     # This is what we return at the end 
     model = {} 
  
-    # Gradient descent. For each batch... 
+    # Gradient descent for each batch
     for i in range(0, num_passes): 
  
         # Forward propagation 
@@ -130,22 +114,18 @@ def build_model(nn_hdim, num_passes=20000, print_loss=False):
         # Assign new parameters to the model 
         model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2} 
  
-        # Optionally print the loss. 
-        # This is expensive because it uses the whole dataset, so we don't want to do it too often. 
+        # Optionally print the loss.
         if print_loss and i % 1000 == 0: 
           print("Loss after iteration %i: %f" %(i, calculate_loss(model))) 
  
     return model 
- 
-# %% 17 
+
 # Build a model with a 3-dimensional hidden layer 
 model = build_model(3, print_loss=True) 
  
 # Plot the decision boundary 
 plot_decision_boundary(lambda x: predict(model, x)) 
 plt.title("Decision Boundary for hidden layer size 3") 
- 
-# %% 14 
 plt.figure(figsize=(16, 32)) 
 hidden_layer_dimensions = [1, 2, 3, 4, 5, 20, 50] 
 for i, nn_hdim in enumerate(hidden_layer_dimensions): 
